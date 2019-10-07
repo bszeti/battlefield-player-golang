d9153cc6dd54462b80c46c1b67f1907db9890468
bszeti <bszeti@redhat.com>
2019-10-07 00:18:38 -0400

    Add scheduled task

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	// "io/ioutil"
+	"time"
 	"log"
 	"net/http"
+	"strings"
+
+	"math/rand"
 
 	"github.com/gorilla/mux"
 	"github.com/bszeti/battlefield-player-golang/services"
@@ -42,25 +45,69 @@ func getCurrentHealth(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(currentHealth) 
 }
 
+var players []string 
+var myName string
+var hitPeriod int
+
 func main() {
 	fmt.Println("Hello World!")
 
+	//Config - maxHealth
 	maxHealth, err := strconv.Atoi( os.Getenv("BATTLEFIELD_MAX_HEALTH") )
 	if (err != nil) {
-		panic(err)
+		log.Fatal("Wrong BATTLEFIELD_MAX_HEALTH")
 	}
 	services.SetCurrentHealth(int32(maxHealth))
 
+	//Config - Hit period
+	hitPeriod, err = strconv.Atoi( os.Getenv("BATTLEFIELD_HIT_PERIOD_MS") )
+	if (err != nil) {
+		log.Fatal("Wrong BATTLEFIELD_HIT_PERIOD_MS")
+	}
+
+	//Config - My name
+	myName = os.Getenv("BATTLEFIELD_PLAYER_NAME")
+	if len(myName)==0 {
+		log.Fatal("Wrong BATTLEFIELD_PLAYER_NAME")
+	}
+
+	//Config - Other players
+	if os.Getenv("BATTLEFIELD_PLAYER_URLS")=="" {
+		log.Fatal("Wrong BATTLEFIELD_PLAYER_URLS")
+	}
+	players = strings.Split(os.Getenv("BATTLEFIELD_PLAYER_URLS"), ",")
+
+	
+	rand.Seed(time.Now().Unix()) // init random generator
+	//Start scheduled service
+	tick := time.NewTicker( time.Duration(hitPeriod) * time.Millisecond )
+	go scheduler(tick)	
+
+
+	//Run http server 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/health", healthHandler)
 	router.HandleFunc("/api/hit/{player}", hitByPlayer).Methods("GET")
 	router.HandleFunc("/api/status/currenthealth", getCurrentHealth).Methods("GET")
-
-		
 
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
         log.Fatal("ListenAndServe Error: ", err)
 	}
 	
+}
+
+func scheduler(tick *time.Ticker){
+	//First time
+	hitRandomPlayer()
+	//When ticks
+	for range tick.C {
+		hitRandomPlayer()
+	}
+}
+
+//Hit another player
+func hitRandomPlayer(){
+	player := players[rand.Intn(len(players))]
+	log.Println("Hitting player",player)
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 export BATTLEFIELD_MAX_HEALTH=3
+export BATTLEFIELD_HIT_PERIOD_MS=2000
+export BATTLEFIELD_PLAYER_NAME=test
+export BATTLEFIELD_PLAYER_URLS=a,b,c
 export TERMINATION_LOG_PATH=./termlog
+ 
 go build && \
 ./battlefield-player-golang 
